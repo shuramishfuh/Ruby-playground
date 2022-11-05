15fd49529f7bfcf2ea595a05fa30b3b2f01d90b7
Ramish <agamatimtim@gmail.com>
2022-11-05 18:31:24 -0500

    Assignment 3 : access modifier for token name and type

--- Assign_3/token.rb
+++ Assign_3/token.rb
@@ -3,7 +3,9 @@
 # CS 3520 Fall 2022
 # this class build a toke based on the name and type
 class Token
+  attr_reader :name, :type
   # initialize method
+
   def initialize(name, type)
     @name = name
     @type = type
